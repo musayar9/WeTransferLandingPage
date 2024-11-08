e5251d151a4c369083790f3d0ba2c43509bc551f
Musa Sayar <musasayar67@gmail.com>
2024-11-08 17:33:09 +0300

    edit mobile navbar icons

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ function toggleMenu(index) {
 
   menuAreas.forEach((menuArea) => {
     const areaIndex = menuArea.getAttribute("data-index");
-    const icon = menuArea.querySelector(".icon-2"); 
+    const icon = menuArea.querySelector(".icon-2");
 
     if (areaIndex === String(index)) {
       menuArea.classList.toggle("active");
-
 
       if (menuArea.classList.contains("active")) {
         icon.classList.add("fa-chevron-up");
@@ -25,4 +24,17 @@ function toggleMenu(index) {
       otherIcon.classList.add("fa-chevron-down");
     }
   });
+}
+
+function dropdownMenu() {
+  const dropdownMobile = document.querySelector(".dropdown-mobile");
+  const iconThree = document.querySelector(".icon-3");
+  dropdownMobile.classList.toggle("mobile-active");
+  if (dropdownMobile.classList.contains("mobile-active")) {
+    iconThree.classList.add("fa-xmark");
+    iconThree.classList.remove("fa-bars");
+  } else {
+    iconThree.classList.remove("fa-xmark");
+    iconThree.classList.add("fa-bars");
+  }
 }
